925ade84f8734fd7437bd303920fdba4efb28c37
onzzzzz <ninaonz.shop@gmail.com>
2024-04-29 10:34:16 +0100

    jbbj

--- routes/privado.js
+++ routes/privado.js
@@ -2,9 +2,9 @@ const privadoRouter = require('express').Router();
 
 
 // Define uma rota para a página HTML
-privadoRouter.get('/gerirCarros', (req, res) => {
+privadoRouter.get('/', (req, res) => {
     // Envie o arquivo HTML como resposta para a solicitação HTTP
-    res.sendFile('/templates/backOffice/tabelaCarros.html');
+    res.sendFile('/Users/ninatchernega/Desktop/web api/AW-P-EXEMPLO-31025-/templates/backOffice/tabelaCarros.html');
   });
 
 
--- routes/publico.js
+++ routes/publico.js
@@ -4,7 +4,7 @@ const publicoRouter = require('express').Router();
 // Define uma rota para a página HTML
 publicoRouter.get('/', (req, res) => {
     // Envie o arquivo HTML como resposta para a solicitação HTTP
-    res.sendFile('/templates/frontEnd/index.html');
+    res.sendFile('/Users/ninatchernega/Desktop/web api/AW-P-EXEMPLO-31025-/templates/frontEnd/index.html');
   });
 
 
